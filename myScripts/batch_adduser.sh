965ad581cf93b8c1ae7a5a2096b206acc5b4dace
haidaoxiaofei <haidaoxiaofei@gmail.com>
2018-11-28 17:10:37 +0800

    save

---
+++ myScripts/batch_adduser.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -f $1 ]
+then
+for i in $(cat $1)
+do
+if [ -f /usr/bin/pwgen ]
+then
+PASSWORD=$(pwgen -1 -s 16)
+else
+PASSWORD=$(cat /dev/urandom | tr -dc "passwordNSR!@#$%0-9" | fold -w 9 | head -1)
+fi
+adduser --quiet --disabled-password  --gecos "" $i
+echo -e "$PASSWORD\n$PASSWORD\n" | passwd --quiet $i
+echo "$i --------- $PASSWORD" >>./userlist-created
+done
+else
+echo "File userlist not found"
+fi
---
+++ myScripts/package_init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# ubuntu_packages.sh
+ 
+# Check if user is root
+if [ $(id -u) != "0" ]; then
+    echo "Error: You must be root to run this script, please use root to install lnmp"
+    exit 1
+fi
+ 
+apt-get update
+apt-get autoremove -y
+apt-get -fy install
+apt-get install -y build-essential gcc g++ make
+for packages in build-essential gcc g++ make automake autoconf re2c wget cron bzip2 libzip-dev libc6-dev file rcconf flex vim nano bison m4 gawk less make cpp binutils diffutils unzip tar bzip2 libbz2-dev unrar p7zip libncurses5-dev libncurses5 libncurses5-dev libncurses5-dev libtool libevent-dev libpcre3 libpcre3-dev libpcrecpp0  libssl-dev zlibc openssl libsasl2-dev libxml2 libxml2-dev libltdl3-dev libltdl-dev libmcrypt-dev libmysqlclient15-dev zlib1g zlib1g-dev libbz2-1.0 libbz2-dev libglib2.0-0 libglib2.0-dev libpng3 libfreetype6 libfreetype6-dev libjpeg62 libjpeg62-dev libjpeg-dev libpng-dev libpng12-0 libpng12-dev curl libcurl3 libmhash2 libmhash-dev libpq-dev libpq5 gettext libncurses5-dev libcurl4-gnutls-dev libjpeg-dev libpng12-dev libxml2-dev zlib1g-dev libfreetype6 libfreetype6-dev libssl-dev libcurl3 libcurl4-openssl-dev libcurl4-gnutls-dev mcrypt libcap-dev;
+do apt-get install -y $packages --force-yes;apt-get -fy install;apt-get -y autoremove; done
