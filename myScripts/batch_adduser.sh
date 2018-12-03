ccabf389740d4515ca0dd7604b3c33488a63b692
haidaoxiaofei <haidaoxiaofei@gmail.com>
2018-12-03 18:38:52 +0800

    add vnc scripts

--- myScripts/batch_adduser.sh
+++ myScripts/batch_adduser.sh
@@ -12,6 +12,7 @@ PASSWORD=$(cat /dev/urandom | tr -dc "passwordNSR!@#$%0-9" | fold -w 9 | head -1
 fi
 adduser --quiet --disabled-password  --gecos "" $i
 echo -e "$PASSWORD\n$PASSWORD\n" | passwd --quiet $i
+sudo usermod -a -G docker $i
 echo "$i --------- $PASSWORD" >>./userlist-created
 done
 else
---
+++ startVNC.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+vncserver :11 -geometry 1680x1050 -localhost
